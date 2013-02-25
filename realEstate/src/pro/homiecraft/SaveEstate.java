300e644c769ae0961b84c3c2c9a9adf0c91b9fd9
Stian T. Home <stitofte@yahoo.no>
2013-02-25 18:13:17 +0100

    Making progress on the save. And getting the WorldEdit selection.

--- realEstate/src/pro/homiecraft/SaveEstate.java
+++ realEstate/src/pro/homiecraft/SaveEstate.java
@@ -1,10 +1,14 @@
 package pro.homiecraft;
 
+import org.bukkit.entity.Player;
+
+import com.sk89q.worldedit.BlockVector;
+
 import pro.homiecraft.config.EstateConfig;
 
 public class SaveEstate {
 	
-	public void estateSave(){
+	public static void estateSave(BlockVector min, BlockVector max, String name, String owner, Player player, String price){
 		boolean saveType = EstateConfig.getEstateConfig("estates").getBoolean("RealEstate.mysql.Use mysql", false);
 		
 		if (saveType == true){
--- realEstate/src/pro/homiecraft/SignListn.java
+++ realEstate/src/pro/homiecraft/SignListn.java
@@ -6,27 +6,57 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+
 public class SignListn implements Listener {
 	@EventHandler
 	public void signCreate(SignChangeEvent event){
 		Player player = event.getPlayer();
 		Block block = event.getBlock();
 		
+		WorldEditPlugin worldEditPlugin = null;
+		worldEditPlugin = (WorldEditPlugin) realEstate.pluginST.getServer().getPluginManager().getPlugin("WorldEdit");
+		Selection sel = worldEditPlugin.getSelection(player);
+		
 		if (realEstate.perms.has(player, "estate.create")){
-			String[] line = event.getLines();
+			if (!(sel == null)){
+				String[] line = event.getLines();
 						
-			if (line[0].equalsIgnoreCase("[estate]")){
-				player.sendMessage("You have created an estate sign!! testy testy :)");
-					
-				event.setLine(0, "[Estate]");
-				event.setLine(1, "No Owner");
-				event.setLine(2, "Testy2");
-				event.setLine(3, "Testy3");
-					
-				block.getState().update();
+				if (line[0].equalsIgnoreCase("[estate]")){
+					if (event.getLine(2) == ""){
+						player.sendMessage("[RealEstate] please specify a price on the third line!");
+					}else{
+						if(event.getLine(3) == ""){
+							player.sendMessage("[RealEstate] please specify a name on the fourth line!");	
+						}else{
+							player.sendMessage("[RealEstate] You have created an estate sign!! testy testy :)");
+							
+							event.setLine(0, "[Estate]");
+							event.setLine(1, "No Owner");
+											
+							String owner = event.getLine(1);
+							String price = event.getLine(2);
+							String name = event.getLine(3);
+							
+							if (sel instanceof CuboidSelection) {
+								BlockVector min = sel.getNativeMinimumPoint().toBlockVector();
+								BlockVector max = sel.getNativeMaximumPoint().toBlockVector();
+								
+								SaveEstate.estateSave(min, max, name, owner, player, price);
+							}
+						
+							block.getState().update();
+						}
+					}
+				}else{
+					player.sendMessage("You don't have permissions to create estates!");
+				}
+			}else{
+				player.sendMessage("You need to have a region selected!");
 			}
-		}else{
-			player.sendMessage("You don't have permissions to create estates!");
 		}
 	}
 }
